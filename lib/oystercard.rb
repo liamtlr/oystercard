5128e1b4a86028aea8bc80f3b7e0c6386cddb0aa
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-03 14:55:53 +0100

    Deduct method created

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,19 @@ class Oystercard
 
   attr_reader :balance
 
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(val)
+    raise "Maxmimum card balance is £#{MAX_BALANCE}" if (@balance + val) > MAX_BALANCE
     @balance += val
   end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
 end
