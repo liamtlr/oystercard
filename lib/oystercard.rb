100473ee6e678afdd17f51b666ca4243178ae38d
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-03 16:53:23 +0100

    Entry station sotred and deleted upon touching in and out; some refactoring

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,13 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   MAX_BALANCE = 90
   MIN_FARE = 1
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station = nil
   end
 
   def top_up(val)
@@ -15,17 +15,19 @@ class Oystercard
     @balance += val
   end
 
-  def touch_in
+  def touch_in(station)
     raise "Insufficient funds: please top up" if @balance < MIN_FARE
-    @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
-    deduct; @in_journey = false
+    deduct
+    @entry_station = nil
+
   end
 
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
 
   private
